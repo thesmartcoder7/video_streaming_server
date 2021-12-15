a787455856670d72df12dc62b3fdd10f749e0a39
Samuel Martins <samuel.martins4.sm@gmail.com>
2021-12-15 04:21:21 +0300

    this is a simple implementation of a video streaming server with Node.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", function (req, res) {
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+
+    const videoPath = "Chris-Do.mp4";
+    const videoSize = fs.statSync("Chris-Do.mp4").size;
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+});
+
+app.listen(8000, function () {
+    console.log("Listening on port 8000!");
+});
